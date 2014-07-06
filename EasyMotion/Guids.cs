28ef1af53bbd13b741533900e97a32a8846a36d1
Jared Parsons <jaredpar@microsoft.com>
2014-07-06 12:03:39 -0700

    Creation of EasyMotion project
    This is just the standard VS Package template.  Lots of changes need to
    go into the spit.  But want to get the initial spit committed so I can
    see what I'm changing

---
+++ EasyMotion/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace JaredPar.EasyMotion
+{
+    static class GuidList
+    {
+        public const string guidEasyMotionPkgString = "964abc60-3497-47df-9eee-029d3085c9fc";
+        public const string guidEasyMotionCmdSetString = "1ddfffc7-09e1-4c00-ba6e-a2d02e217fd1";
+
+        public static readonly Guid guidEasyMotionCmdSet = new Guid(guidEasyMotionCmdSetString);
+    };
+}
