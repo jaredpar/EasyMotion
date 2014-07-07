e2e939b92e451764763d455c875d9e2ba22278eb
Jared Parsons <jaredpar@microsoft.com>
2014-07-06 18:22:31 -0700

    Added a status margin
    The status margin appears during a easy motion operation.  It gives
    hints as to the next step for the user.

---
+++ EasyMotion/Implementation/Margin/EasyMotionMarginController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace EasyMotion.Implementation.Margin
+{
+    internal sealed class EasyMotionMarginController : IWpfTextViewMargin
+    {
+        private readonly IEasyMotionUtil _easyMotionUtil;
+        private readonly EasyMotionMargin _control;
+
+        internal EasyMotionMarginController(IEasyMotionUtil easyMotionUtil)
+        {
+            _easyMotionUtil = easyMotionUtil;
+            _easyMotionUtil.StateChanged += OnStateChanged;
+            _control = new EasyMotionMargin();
+        }
+
+        private void Unsubscribe()
+        {
+            _easyMotionUtil.StateChanged -= OnStateChanged;
+        }
+
+        private void OnStateChanged(object sender, EventArgs e)
+        {
+            switch (_easyMotionUtil.State)
+            {
+                case EasyMotionState.Disabled:
+                    _control.Visibility = Visibility.Collapsed;
+                    break;
+                case EasyMotionState.LookingForChar:
+                    _control.Visibility = Visibility.Visible;
+                    _control.StatusLine = "Type the character you want to search for";
+                    break;
+                case EasyMotionState.LookingForDecision:
+                    _control.Visibility = Visibility.Visible;
+                    _control.StatusLine = "Type the character at the location you want to jump to";
+                    break;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+        }
+
+        public bool Enabled  
+        {
+            get { return _easyMotionUtil.State != EasyMotionState.Disabled; }
+        }
+
+        public double MarginSize
+        {
+            get { return 25; }
+        }
+
+        public FrameworkElement VisualElement
+        {
+            get { return _control; }
+        }
+
+        public void Dispose()
+        {
+            Unsubscribe();
+        }
+
+        public ITextViewMargin GetTextViewMargin(string marginName)
+        {
+            return EasyMotionMarginProvider.Name == marginName ? this : null;
+        }
+    }
+}
---
+++ EasyMotion/Implementation/Margin/EasyMotionMarginProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace EasyMotion.Implementation.Margin
+{
+    [Export(typeof(IWpfTextViewMarginProvider))]
+    [MarginContainer(PredefinedMarginNames.Bottom)]
+    [ContentType("any")]
+    [Name(Name)]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    internal sealed class EasyMotionMarginProvider : IWpfTextViewMarginProvider
+    {
+        internal const string Name = "Easy Motion Margin";
+
+        private readonly IEasyMotionUtilProvider _easyMotionUtilProvider;
+
+        [ImportingConstructor]
+        internal EasyMotionMarginProvider(IEasyMotionUtilProvider easyMotionUtilProvider)
+        {
+            _easyMotionUtilProvider = easyMotionUtilProvider;
+        }
+
+        public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer)
+        {
+            var easyMotionUtil = _easyMotionUtilProvider.GetEasyMotionUtil(wpfTextViewHost.TextView);
+            return new EasyMotionMarginController(easyMotionUtil);
+        }
+    }
+}
